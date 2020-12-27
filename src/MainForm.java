b155c69a8069cfb918b8977ba4369fe4fc73be24
liza <kozhliza@gmail.com>
2020-12-27 14:30:09 +0200

    ui updated

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,5 +1,7 @@
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
@@ -8,13 +10,21 @@ import java.util.logging.Logger;
 public class MainForm {
 
     private JPanel mainPanel;
-
-    public MainForm() {
-
-    }
+    private JButton start;
+    private JTextField floors;
+    private JButton addElevatorButton;
+    private JTextArea elevatorList;
+    private JTextField spawnRate;
 
 
     public static void main(String[] args){
+
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(new MainForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(700, 400);
+        frame.setVisible(true);
 
         EventLogger logger = new EventLogger("log.txt");
         Building building = new Building(logger, 300, 10);
